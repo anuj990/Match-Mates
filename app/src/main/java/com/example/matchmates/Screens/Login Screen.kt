4ed6765eeb95ab153a59c8191c7797c832d289c4
anuj990 <gp712223@gmail.com>
2025-09-05 14:37:15 +0530

    First commit

--- app/src/main/java/com/example/matchmates/Screens/Login Screen.kt
+++ app/src/main/java/com/example/matchmates/Screens/Login Screen.kt
@@ -1,9 +1,12 @@
 package com.example.matchmates.Screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {
+    Column {
 
+    }
 }
