7b3052730e851908995f9ee3b9005cb6eb08a7f3
Manan Maheshwari <mananmaheshwariji@gmail.com>
2025-09-05 19:02:22 +0530

    Added Skill Registration Screen

--- app/src/main/java/com/example/matchmates/Screens/RegistrationScreen.kt
+++ app/src/main/java/com/example/matchmates/Screens/RegistrationScreen.kt
@@ -14,7 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.material3.MenuAnchorType // ✅ new anchor type
+import androidx.compose.material3.MenuAnchorType
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
